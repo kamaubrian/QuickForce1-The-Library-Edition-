bed71e1f30adcd5d4fe87886a2e6db4fde193009
mtotodev05 <mtotodev05@gmail.com>
2017-09-16 00:39:58 +0300

    database params

--- src/main/java/Model/mainModel.java
+++ src/main/java/Model/mainModel.java
@@ -1,5 +1,10 @@
 package Model;
+
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 /**
  * Created by Brian Kamau on 9/16/17.;
  */
@@ -13,5 +18,45 @@ public abstract class mainModel implements modelinterface{
     protected Connection conn =null;
     protected ResultSet rst=null;
 
+
+    @Override
+    public final boolean dbConnect(){
+
+        boolean success=true;
+        try{
+            Class.forName(dbDriver);
+            conn=DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+            System.out.println("Successful Connection to Database");
+            success=true;
+
+        }catch(Exception ex){
+
+            System.err.println("Database Connection Error"+ex.getMessage());
+            success = false;
+        }
+
+        return success;
+    }
+    @Override
+    public final boolean dbDisconnect(){
+        boolean success = true;
+        try{
+            if(rst!=null){
+                rst.close();
+            }
+            if(conn!=null){
+                conn.close();
+
+            }if(pst!=null){
+                pst.close();
+            }
+
+        }catch(Exception ex){
+            System.out.println("Closing Database Exception"+ex.getMessage());
+        }
+
+
+        return success;
+    }
 
 }
