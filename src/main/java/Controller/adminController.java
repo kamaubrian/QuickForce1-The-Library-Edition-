8c6c72bf30e18f83002adfeb0da65be1fe142011
mtotodev05 <mtotodev05@gmail.com>
2017-09-16 02:47:11 +0300

    Login Successful

--- src/main/java/Controller/adminController.java
+++ src/main/java/Controller/adminController.java
@@ -1,14 +1,17 @@
 package Controller;
+import java.io.IOException;
 import java.util.Scanner;
+import Model.*;
 /**
  * Created by root on 9/16/17.
  */
 public class adminController implements admininterface {
     static Scanner scan = new Scanner(System.in);
+    static adminModel adminmodel = new adminModel();
     static String username;
     static String password;
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
 
         System.out.println("\t\tLibrary Management System");
         System.out.println("Enter Login Details\n");
@@ -16,20 +19,22 @@ public class adminController implements admininterface {
         username=scan.next();
         System.out.println("Enter Password");
         password=scan.next();
-
+        login(username,password);
 
     }
 
+    @org.jetbrains.annotations.Contract(pure = true)
+    public static boolean login(String username, String password) throws IOException{
+        boolean sucess = true;
+        if(adminmodel.getUsername(username).equals(username) && adminmodel.getPassword(password).equals(password)){
+            System.out.println("Welcome to Our Library");
+        }else{
+            System.out.println("Incorrect Credentials");
+            sucess=false;
+        }
 
-    @Override
-    public boolean login(String username, String password){
 
-
-
-
-
-
-        return false;
+        return sucess;
     }
 
 
--- src/main/java/Controller/admininterface.java
+++ src/main/java/Controller/admininterface.java
@@ -4,7 +4,7 @@ package Controller;
  * Created by root on 9/16/17.
  */
 public interface admininterface {
-    public boolean login(String username,String password);
+   // public  boolean login(String username,String password);
 
 
 }
