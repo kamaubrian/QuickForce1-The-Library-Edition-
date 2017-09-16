430fd2f615b34e0705f009abca7d1107e05b46a8
mtotodev05 <mtotodev05@gmail.com>
2017-09-16 21:00:26 +0300

    Schools Controllers

---
+++ src/main/java/Controller/schoolsController.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by root on 9/16/17.
+ */
+public class schoolsController {
+}
---
+++ src/main/java/Model/schoolsModel.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by root on 9/16/17.
+ */
+public class schoolsModel extends mainModel {
+}
